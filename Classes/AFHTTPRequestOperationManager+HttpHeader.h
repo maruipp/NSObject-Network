ca74ff63d2b04e405673b2517b21affa5265302d
myron <maruipeng@ijiwai.com>
2015-09-18 22:05:14 +0800

    修改README

---
+++ Classes/AFHTTPRequestOperationManager+HttpHeader.h
@@ -0,0 +1,13 @@
+//
+//  AFHTTPRequestOperationManager+YoucheHeader.h
+//  youche
+//
+//  Created by 马 瑞鹏 on 14-4-26.
+//  Copyright (c) 2014年 YintaiYouChe. All rights reserved.
+//
+
+#import "AFHTTPRequestOperationManager.h"
+
+@interface AFHTTPRequestOperationManager (YoucheHeader)
+- (void)addYoucheRequestHeader;
+@end
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 use category to enhance nsobject, make it easy to use afnetwork
 ================
+###Pod引入方式：
+在PodFile文件中添加：
+
+pod "NSObject-Network", :git => 'git@github.com:maruipp/NSObject-Network.git', :branch => 'master'
